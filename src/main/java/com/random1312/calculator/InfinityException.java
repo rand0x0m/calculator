54555261bcd29be55917064a3b00aa66766a608b
random1312 <random1312@users.noreply.github.com>
2017-04-24 11:21:55 +0300

    Minor fix.

--- src/main/java/com/random1312/calculator/InfinityException.java
+++ src/main/java/com/random1312/calculator/InfinityException.java
@@ -3,6 +3,8 @@ package com.random1312.calculator;
 
 public class InfinityException extends Exception {
 	
+	private static final long serialVersionUID = -2842263184683211290L;
+	
 	public InfinityException() {
 		super();
 	}
